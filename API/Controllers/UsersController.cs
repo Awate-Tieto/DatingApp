310fed1d11ccbbed614c3bc460eaa97b0785a4a9
Awate-Tieto <124045905+Awate-Tieto@users.noreply.github.com>
2023-02-06 10:47:32 +0000

    Latest stuff from section1Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+
+            var users = await _context.Users.ToListAsync();
+
+            if(users.Any())
+            {
+                return users;
+            }else {
+
+                return BadRequest("Finns inga users");
+            }
+
+            
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+
+            return await _context.Users.FindAsync(id);
+
+        }
+
+    }
+}
